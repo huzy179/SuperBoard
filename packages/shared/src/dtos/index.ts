e5d0b15924d354d8947d0dd0a6d25475e106683b
huzy179 <maitranhuy1709@gmail.com>
2026-03-20 20:24:01 +0700

    chore(repo): stabilize pre-commit lint scope and shared dto exports

--- packages/shared/src/dtos/index.ts
+++ packages/shared/src/dtos/index.ts
@@ -1,4 +1,15 @@
 export * from './auth.dto';
 export * from './health.dto';
 export * from './project.dto';
-export * from './workflow.dto';
+export type {
+  ArchiveWorkspaceRequestDTO,
+  RestoreWorkspaceRequestDTO,
+  ArchiveProjectRequestDTO,
+  RestoreProjectRequestDTO,
+  ArchiveTaskRequestDTO,
+  RestoreTaskRequestDTO,
+  WorkflowListFilterRequestDTO,
+  LifecycleOperationResultDTO,
+  ArchiveWorkspaceResponseDTO,
+  RestoreWorkspaceResponseDTO,
+} from './workflow.dto';
